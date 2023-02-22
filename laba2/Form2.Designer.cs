e263e917146bcdefd28a9490e538bd9075d3a09b
Ezvover <vovasik273@gmail.com>
2023-02-23 00:36:25 +0300

    Добавьте файлы проекта.

---
+++ laba2/Form2.Designer.cs
@@ -0,0 +1,462 @@
+﻿namespace laba2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(114, 118);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(97, 26);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Intel Core i5",
+            "Intel Core i7",
+            "Intel Core i9",
+            "Intel Xeon",
+            "AMD Ryzen Threadripper",
+            "AMD Ryzen 5",
+            "AMD Ryzen 7",
+            "AMD Ryzen 9"});
+            this.comboBox1.Location = new System.Drawing.Point(111, 282);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 467);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ОЗУ:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(111, 513);
+            this.trackBar1.Maximum = 1024;
+            this.trackBar1.Minimum = 8;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(85, 45);
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.Value = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "NVIDIA RTX 2050",
+            "NVIDIA RTX 2060",
+            "NVIDIA RTX 2070",
+            "NVIDIA RTX 2080",
+            "NVIDIA RTX 4060",
+            "NVIDIA RTX 4070",
+            "NVIDIA RTX 4080",
+            "NVIDIA RTX A5000",
+            "NVIDIA RTX A5500",
+            "NVIDIA RTX A6000",
+            "NVIDIA RTX A6500 ADA",
+            "AMD Radeon Pro W6800"});
+            this.comboBox2.Location = new System.Drawing.Point(108, 431);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(111, 579);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 19);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Micro";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(111, 604);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 19);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Portable";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(111, 628);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(69, 19);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Standart";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(108, 702);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 19);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "HDD";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(108, 727);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 19);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "SSD";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(108, 787);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "ПК",
+            "Ноутбук",
+            "Сервер"});
+            this.comboBox3.Location = new System.Drawing.Point(114, 44);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 23);
+            this.comboBox3.TabIndex = 22;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(648, 159);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(399, 441);
+            this.richTextBox2.TabIndex = 25;
+            this.richTextBox2.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1703, 487);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 15);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "L3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1703, 434);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 15);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "L2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1703, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 15);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "L1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1721, 577);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1700, 509);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 36;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1700, 461);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 35;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1700, 398);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 34;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(1721, 318);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(43, 19);
+            this.radioButton4.TabIndex = 33;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "x64";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(1721, 293);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(43, 19);
+            this.radioButton5.TabIndex = 32;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "x32";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "2.2",
+            "2.4",
+            "2.6",
+            "2.8",
+            "3.0",
+            "3.2",
+            "3.4",
+            "3.6",
+            "3.8",
+            "4.0"});
+            this.comboBox4.Location = new System.Drawing.Point(1686, 243);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 23);
+            this.comboBox4.TabIndex = 31;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1721, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "label5";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 16;
+            this.trackBar2.Location = new System.Drawing.Point(1686, 192);
+            this.trackBar2.Maximum = 16;
+            this.trackBar2.Minimum = 4;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(121, 45);
+            this.trackBar2.TabIndex = 29;
+            this.trackBar2.Value = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "5",
+            "7",
+            "9",
+            "Server"});
+            this.comboBox5.Location = new System.Drawing.Point(1686, 122);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 23);
+            this.comboBox5.TabIndex = 28;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(1721, 75);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(52, 19);
+            this.radioButton6.TabIndex = 27;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "AMD";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(1721, 50);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(48, 19);
+            this.radioButton7.TabIndex = 26;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Intel";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2030, 937);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private ComboBox comboBox1;
+        private Label label1;
+        private TrackBar trackBar1;
+        private ComboBox comboBox2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private DateTimePicker dateTimePicker1;
+        private ComboBox comboBox3;
+        private Button button2;
+        private RichTextBox richTextBox2;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button button3;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private ComboBox comboBox4;
+        private Label label5;
+        private TrackBar trackBar2;
+        private ComboBox comboBox5;
+        private RadioButton radioButton6;
+        private RadioButton radioButton7;
+    }
+}
---
+++ laba2/Form2.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2
+{
+    public partial class Form2 : Form
+    {
+        Computer computer;
+
+        public Form2()
+        {
+            InitializeComponent();
+            trackBar1.Scroll+= trackBar1_Scroll;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = String.Format($"Необходимое ОЗУ: {trackBar1.Value} ГБ");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+  
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label5.Text = String.Format($"Количество ядер: {trackBar2.Value}");
+
+        }
+
+        public void ToClass()
+        {
+            // type radiobutton
+            if (radioButton1.Checked)
+            {
+                computer.type = radioButton1.Text;
+            }
+            else if (radioButton2.Checked)
+            {
+                computer.type = radioButton2.Text;
+            }
+            else if (radioButton3.Checked) 
+            {
+                computer.type = radioButton3.Text;
+            }
+
+            if (radioButton7.Checked)
+            {
+                computer.creator = radioButton5.Text;
+            }
+            else if (radioButton6.Checked)
+            {
+                computer.creator = radioButton6.Text;
+            }
+
+            computer.series = comboBox1.Text;
+
+            computer.model = comboBox2.Text;
+
+            computer.cores = trackBar2.Value;
+
+            computer.clock = comboBox3.Text;
+
+            if (radioButton4.Checked)
+            {
+                computer.architecture = radioButton5.Text;
+            }
+            else if (radioButton5.Checked) 
+            {
+                computer.architecture = radioButton6.Text;
+            }
+
+            try
+            {
+                computer.l1Cache = int.Parse(textBox1.Text);
+                computer.l2Cache = int.Parse(textBox2.Text);
+                computer.l3Cache = int.Parse(textBox3.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (radioButton1.Checked)
+            {
+                computer.size= radioButton1.Text;
+            }
+            else if(radioButton2.Checked) 
+            {
+                computer.size = radioButton2.Text;
+            }
+            else if(radioButton3.Checked)
+            {
+                computer.size = radioButton3.Text;
+            }
+
+            computer.RAM = trackBar1.Value;
+
+            if (checkBox1.Checked) 
+            {
+                computer.drive = radioButton1.Text;
+            }
+            else if (checkBox2.Checked)
+            {
+                computer.drive = radioButton2.Text;
+            }
+            else if (checkBox1.Checked && checkBox2.Checked)
+            {
+                computer.drive = $"{checkBox1.Text}, {checkBox2.Text}";
+            }
+
+            computer.date = dateTimePicker1.Text;
+
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ToClass();
+            string str = computer.type + computer.creator + computer.series + computer.model + computer.cores + computer.clock + computer.architecture + computer.l1Cache + computer.l2Cache + computer.l3Cache + computer.size + computer.RAM + computer.drive + computer.date;
+            richTextBox2.Text = str;
+        }
+
+        public struct Computer
+        {
+            public string type;
+            public string creator;
+            public string series;
+            public string model;
+            public int cores;
+            public string clock;
+            public string architecture;
+            public int l1Cache;
+            public int l2Cache;
+            public int l3Cache;
+
+            public string GPU;
+            public string size;
+            public int RAM;
+            public string drive;
+            public string date;
+
+
+        }
+    }
+
+   
+}
