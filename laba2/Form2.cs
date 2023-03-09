9b1cca81449037db62f5be39d3e328410e66bd3b
Vover R <vovasik273@gmail.com>
2023-03-09 17:36:01 +0300

    Added serialization

--- laba2/Form2.cs
+++ laba2/Form2.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.Devices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace laba2
 {
@@ -16,25 +20,48 @@ namespace laba2
 
         public struct CPU
         {
-            public string creator;
-            public string series;
-            public string model;
-            public int cores;
-            public string clock;
-            public string architecture;
-            public int l1Cache;
-            public int l2Cache;
-            public int l3Cache;
+            public CPU(string creator, string series, string model, int cores, string clock, string architecture, int l1Cache, int l2Cache, int l3Cache) 
+            {
+                Creator= creator;
+                Series= series;
+                Model= model;
+                Cores= cores;
+                Clock= clock;
+                Architecture= architecture;
+                L1Cache = l1Cache;
+                L2Cache = l2Cache;
+                L3Cache= l3Cache;
+            }
+
+            public string Creator { get; set; }
+            public string Series { get; set; }
+            public string Model { get; set; }
+            public int Cores { get; set; }
+            public string Clock { get; set; }
+            public string Architecture { get; set; }
+            public int L1Cache { get; set; }
+            public int L2Cache { get; set; }
+            public int L3Cache { get; set; }
         }
 
         public struct GPU
         {
-            public string creator;
-            public string series;
-            public string model;
-            public int clock;
-            public string diretX;
-            public int GPURAM;
+            public GPU(string creator, string series, string model, int clock, string diretx, int gpuRAM)
+            {
+                Creator= creator;
+                Series= series;
+                Model= model;
+                Clock = clock;
+                DiretX = diretx;
+                GPURAM = gpuRAM;
+            }
+
+            public string Creator { get; set; }
+            public string Series { get; set; }
+            public string Model { get; set; } 
+            public int Clock { get; set; }
+            public string DiretX { get; set; }
+            public int GPURAM { get; set; }
         }
 
         public Form2()
@@ -48,13 +75,6 @@ namespace laba2
             label1.Text = String.Format($"Необходимое ОЗУ: {RamAmount.Value} ГБ");
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-  
-
         private void trackBar2_Scroll(object sender, EventArgs e)
         {
             label5.Text = String.Format($"Количество ядер: {CoresChoose.Value}");
@@ -64,39 +84,39 @@ namespace laba2
         public void ToClass()
         {
             // type radiobutton
-            computer.type = ComputerType.Text;
+            computer.Type = ComputerType.Text;
 
             if (CreatorIntel.Checked)
             {
-                computer.cpu.creator = CreatorIntel.Text;
+                computer.cpu.Creator = CreatorIntel.Text;
             }
             else if (CreatorAMD.Checked)
             {
-                computer.cpu.creator = CreatorAMD.Text;
+                computer.cpu.Creator = CreatorAMD.Text;
             }
 
-            computer.cpu.series = SeriesChoose.Text;
+            computer.cpu.Series = SeriesChoose.Text;
 
-            computer.cpu.model = comboBox1.Text;
+            computer.cpu.Model = comboBox1.Text;
 
-            computer.cpu.cores = CoresChoose.Value;
+            computer.cpu.Cores = CoresChoose.Value;
 
-            computer.cpu.clock = ClockChoose.Text;
+            computer.cpu.Clock = ClockChoose.Text;
 
             if (ArchitectureX32.Checked)
             {
-                computer.cpu.architecture = ArchitectureX32.Text;
+                computer.cpu.Architecture = ArchitectureX32.Text;
             }
             else if (ArchitectureX64.Checked)
             {
-                computer.cpu.architecture = ArchitectureX64.Text;
+                computer.cpu.Architecture = ArchitectureX64.Text;
             }
 
             try
             {
-                computer.cpu.l1Cache = int.Parse(textBox1.Text);
-                computer.cpu.l2Cache = int.Parse(textBox2.Text);
-                computer.cpu.l3Cache = int.Parse(textBox3.Text);
+                computer.cpu.L1Cache = int.Parse(textBox1.Text);
+                computer.cpu.L2Cache = int.Parse(textBox2.Text);
+                computer.cpu.L3Cache = int.Parse(textBox3.Text);
             }
             catch (Exception ex)
             {
@@ -105,55 +125,55 @@ namespace laba2
 
             if (MicroRadio.Checked)
             {
-                computer.size = MicroRadio.Text;
+                computer.Size = MicroRadio.Text;
             }
             else if (PortableRadio.Checked)
             {
-                computer.size = PortableRadio.Text;
+                computer.Size = PortableRadio.Text;
             }
             else if (StandartRadio.Checked)
             {
-                computer.size = StandartRadio.Text;
+                computer.Size = StandartRadio.Text;
             }
 
             computer.RAM = RamAmount.Value;
 
             if (checkBox1.Checked)
             {
-                computer.drive = checkBox1.Text;
+                computer.Drive = checkBox1.Text;
             }
             else if (checkBox2.Checked)
             {
-                computer.drive = checkBox2.Text;
+                computer.Drive = checkBox2.Text;
             }
             else if (checkBox1.Checked && checkBox2.Checked)
             {
-                computer.drive = $"{checkBox1.Text}, {checkBox2.Text}";
+                computer.Drive = checkBox1.Text + ", " + checkBox2.Text;
             }
 
-            computer.date = dateTimePicker1.Text;
+            computer.Date = dateTimePicker1.Text;
 
 
             if (NvidiaGPU.Checked)
             {
-                computer.gpu.creator = NvidiaGPU.Text;
+                computer.gpu.Creator = NvidiaGPU.Text;
             }
             else if (AmdGPU.Checked)
             {
-                computer.gpu.creator = AmdGPU.Text;
+                computer.gpu.Creator = AmdGPU.Text;
             }
 
-            computer.gpu.series = GPUSeries.Text;
-            computer.gpu.model = GPUModel.Text;
-            computer.gpu.clock = GpuClock.Value;
+            computer.gpu.Series = GPUSeries.Text;
+            computer.gpu.Model = GPUModel.Text;
+            computer.gpu.Clock = GpuClock.Value;
 
             if (DiretXYes.Checked)
             {
-                computer.gpu.diretX = DiretXYes.Text;
+                computer.gpu.DiretX = DiretXYes.Text;
             }
             else if (DiretXNo.Checked)
             {
-                computer.gpu.diretX = DiretXNo.Text;
+                computer.gpu.DiretX = DiretXNo.Text;
             }
 
             computer.gpu.GPURAM = GpuRAM.Value;
@@ -162,21 +182,57 @@ namespace laba2
         private void button3_Click(object sender, EventArgs e)
         {
             ToClass();
-            string str = computer.type + ' ' + computer.cpu.creator + ' ' + computer.cpu.series + ' ' + computer.cpu.model + ' ' + computer.cpu.cores + ' ' + computer.cpu.clock + ' ' + computer.cpu.architecture + ' ' + computer.cpu.l1Cache + ' ' + computer.cpu.l2Cache + ' ' + computer.cpu.l3Cache + ' ' + computer.size + ' ' + computer.RAM + ' ' + computer.drive + ' ' + computer.date + ' ' + computer.gpu.creator + ' ' + computer.gpu.series + ' ' + computer.gpu.model + ' ' + computer.gpu.clock + ' ' + computer.gpu.diretX + ' ' + computer.gpu.GPURAM;
+            string str = $"Тип: {computer.Type} \nРазмер: {computer.Size} \nДиск: {computer.Drive} \nПроизводитель ЦП: {computer.cpu.Creator} \nМодель ЦП: {computer.cpu.Model} \nКол-во ядер ЦП: {computer.cpu.Cores} \nЧастота ЦП: {computer.cpu.Clock} \nАрхитектура ЦП: {computer.cpu.Architecture} \nL1, L2, L3 кэш: {computer.cpu.L1Cache}, {computer.cpu.L2Cache}, {computer.cpu.L3Cache} \nПроиводитель ГК: {computer.gpu.Creator} \nСерия ГК: {computer.gpu.Series} \nМодель ГК: {computer.gpu.Model} \nЧастота ГК: {computer.gpu.Clock} \nНаличие DiretX: {computer.gpu.DiretX} \nКоличество видеопамяти: {computer.gpu.GPURAM}"; 
             richTextBox2.Text = str;
+            List<Computer> computerList  = new List<Computer>();
+            computerList.Add(new Computer(computer.Type, computer.Size, computer.RAM, computer.Drive, computer.Date, computer.cpu.Creator, computer.cpu.Series, computer.cpu.Model, computer.cpu.Cores, computer.cpu.Clock, computer.cpu.Architecture, computer.cpu.L1Cache, computer.cpu.L2Cache, computer.cpu.L3Cache, computer.gpu.Creator, computer.gpu.Series, computer.gpu.Model, computer.gpu.Clock, computer.gpu.DiretX, computer.gpu.GPURAM));
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Computer));
+            using (FileStream stream = new FileStream("computer.xml", FileMode.OpenOrCreate))
+            {
+                foreach(Computer computer in computerList) 
+                {
+                    xmlSerializer.Serialize(stream, computer);
+                }
+            }
         }
 
         public struct Computer
         {
+            public Computer(string type, string size, int ram, string drive, string date, string cpuCreator, string cpuSeries, string cpuModel, int cpuCores, string cpuClock, string cpuArchitecture, int cpuL1Cache, int cpuL2Cache, int cpuL3Cache, string gpuCreator, string gpuSeries, string gpuModel, int gpuClock, string gpuDiretx, int gpuRAM)
+            {
+                Type = type;
+                Size = size;
+                RAM = ram;
+                Drive = drive;
+                Date = date;
+
+                cpu.Creator = cpuCreator;
+                cpu.Series = cpuSeries;
+                cpu.Model = cpuModel;
+                cpu.Cores = cpuCores;
+                cpu.Clock = cpuClock;
+                cpu.Architecture = cpuArchitecture;
+                cpu.L1Cache = cpuL1Cache;
+                cpu.L2Cache = cpuL2Cache;
+                cpu.L3Cache = cpuL3Cache;
+
+                gpu.Creator = gpuCreator;
+                gpu.Series = gpuSeries;
+                gpu.Model = gpuModel;
+                gpu.Clock = gpuClock;
+                gpu.DiretX= gpuDiretx;
+                gpu.GPURAM = gpuRAM;
+            }
+
             public CPU cpu;
             public GPU gpu;
 
-            public string type;
-            
-            public string size;
-            public int RAM;
-            public string drive;
-            public string date;
+            public string Type { get; set; }
+
+            public string Size { get; set; }
+            public int RAM { get; set; }
+            public string Drive { get; set; }
+            public string Date { get; set; }
 
 
         }
